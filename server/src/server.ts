2867bb3f32d1df75680b574338b0c132af6fdb08
James Reyes <jamesleoreyes@gmail.com>
2025-05-14 00:04:28 -0400

    refactor: Remove custom logging implementation and simplify server startup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,6 @@
-import { setupCustomLogging } from "./config/logger";
 import app from "./app";
 import appConfig from "./config/app";
 import { isEnvValid, requiredEnvVars } from "./utils/env";
-
-setupCustomLogging(appConfig.env === 'development');
 
 const startServer = () => {
   if (!isEnvValid(requiredEnvVars)) process.exitCode = 1;
